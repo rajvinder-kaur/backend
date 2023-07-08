ba1f394ea401a2a87e9ec7ffcd2367bf0ecc5abf
ace <kaurrajvinder736@gmail.com>
2023-07-08 21:16:32 +0530

    done with the backend

--- server.js
+++ server.js
@@ -11,19 +11,25 @@ const port = process.env.PORT || 9000;
 
 
 const pusher = new Pusher({
-  appId: "1629618",
-  key: "99184aadb6e95ee75def",
-  secret: "e3bc1fce03a44dcb52e1",
-  cluster: "ap2",
-  useTLS: true
+    appId: "1629618",
+    key: "99184aadb6e95ee75def",
+    secret: "e3bc1fce03a44dcb52e1",
+    cluster: "ap2",
+    useTLS: true
 });
-
+// pusher added to the server that syncs the mongodb and reactjs for realtime changes, as it refreshes the frontend for new changes when there is any change detected in database
 pusher.trigger("my-channel", "my-event", {
-  message: "hello world"
+    message: "hello world"
 });
 
 //middleware
 app.use(express.json());
+
+app.use((req,res, next)=>{
+    res.setHeader("Acess-Control-Allow-Origin","*");
+    res.setHeader("Acess-Control-Allow-Headers","*");
+    next();
+});
 
 //DB config
 
@@ -38,29 +44,57 @@ mongoose.connect(
     }
 )
     .then((x) => {
-        console.log("Connected to Mongo!");
+        console.log("Connected to Mongo!" + x);
     })
     .catch((err) => {
-        console.log("Error while connecting to Mongo");
+        console.log("Error while connecting to Mongo" + err);
     });
+
+const con = mongoose.connection
+con.on('open', function () {
+    console.log("finally iti mehnat k baad ");
+    const msgCollection = con.collection("msgcontents");
+    const cs = msgCollection.watch();
+    // console.log(cs);
+    cs.on('change',(change)=>{
+        // console.log(change)
+
+        if(change.operationType === 'insert'){
+            const mesg = change.fullDocument;
+            pusher.trigger('messages', 'inserted',{
+                name: mesg.user,
+                message: mesg.message,
+            });
+            console.log(mesg)
+            //triggers when a message is inserted in db
+        }else{
+            console.log('error')
+        }
+
+    });
+});
+// this function is called change stream that watches changes on mongodb and notifies pusher about them.
+
+
 
 //??
 
 //api routes
 
 
-app.get('/',(req,res)=>{res.status(200).send('heyy')});
+app.get('/', (req, res) => { res.status(200).send('heyy') });
 
-app.post('/messages/new',(req,res)=>{
+app.post('/messages/new', (req, res) => {
     const dbMsg = req.body;
     Messages.create(dbMsg);
 
 });
 
 app.get('/messages/sync',
-    async( req , res)=>{
-     const msg = await Messages.find();
-    console.log(msg)}
+    async (req, res) => {
+        const msg = await Messages.find();
+        console.log(msg)
+    }
 )
 
 
@@ -78,7 +112,3 @@ app.listen(port, () => {
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-// const con = mongoose.connection
-// con.on('open', function () {
-//     console.log("finally iti mehnat k baad ");
-// })
